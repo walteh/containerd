f98dbcb3ff513bec34b5313d4dff0e088d5f96e5
Marat Radchenko <marat@slonopotamus.org>
2023-07-09 16:34:12 +0300

    Add support for bind-mounts on Darwin
    Signed-off-by: Marat Radchenko <marat@slonopotamus.org>

--- core/mount/mount_darwin.go
+++ core/mount/mount_darwin.go
@@ -16,9 +16,39 @@
 
 package mount
 
-import "github.com/containerd/errdefs"
+import (
+	"fmt"
+	"os/exec"
+)
 
 // Mount to the provided target.
 func (m *Mount) mount(target string) error {
-	return errdefs.ErrNotImplemented
+	var commandName string
+	if m.Type == "bind" {
+		// macOS doesn't natively support bindfs/nullfs
+		// The way to emulate it is via FUSE fs named "bindfs"
+		commandName = "bindfs"
+	} else {
+		commandName = fmt.Sprintf("mount_%s", m.Type)
+	}
+
+	var args []string
+	for _, option := range m.Options {
+		if option == "rbind" {
+			// On one side, rbind is not supported by macOS mounting tools
+			// On the other, bindfs works as if rbind is enabled anyway
+			continue
+		}
+
+		args = append(args, "-o", option)
+	}
+	args = append(args, m.Source, target)
+
+	cmd := exec.Command(commandName, args...)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("%s [%v] failed: %q: %w", commandName, args, string(output), err)
+	}
+
+	return nil
 }

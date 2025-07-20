617b740fa9f7f8bf8abbae14f3c09fa619cf4c05
Walter Scott <w@lter.ca>
2025-07-19 22:10:27 -0500

    allow linux opts for darwin

--- pkg/oci/spec_opts_darwin.go
+++ pkg/oci/spec_opts_darwin.go
@@ -1,5 +1,3 @@
-//go:build !linux && !windows && !darwin
-
 /*
    Copyright The containerd Authors.
 
@@ -22,6 +20,8 @@ import (
 	"context"
 
 	"github.com/containerd/containerd/v2/core/containers"
+	"github.com/containerd/containerd/v2/pkg/cap"
+	specs "github.com/opencontainers/runtime-spec/specs-go"
 )
 
 // WithHostDevices adds all the hosts device nodes to the container's spec
@@ -36,20 +36,45 @@ func WithHostDevices(_ context.Context, _ Client, _ *containers.Container, s *Sp
 	return nil
 }
 
-// WithDevices recursively adds devices from the passed in path.
+// WithDevices recursively adds devices from the passed in path and associated cgroup rules for that device.
 // If devicePath is a dir it traverses the dir to add all devices in that dir.
 // If devicePath is not a dir, it attempts to add the single device.
+// If containerPath is not set then the device path is used for the container path.
 func WithDevices(devicePath, containerPath, permissions string) SpecOpts {
 	return func(_ context.Context, _ Client, _ *containers.Container, s *Spec) error {
 		devs, err := getDevices(devicePath, containerPath)
 		if err != nil {
 			return err
 		}
-		s.Linux.Devices = append(s.Linux.Devices, devs...)
+		for i := range devs {
+			s.Linux.Devices = append(s.Linux.Devices, devs[i])
+			s.Linux.Resources.Devices = append(s.Linux.Resources.Devices, specs.LinuxDeviceCgroup{
+				Allow:  true,
+				Type:   devs[i].Type,
+				Major:  &devs[i].Major,
+				Minor:  &devs[i].Minor,
+				Access: permissions,
+			})
+		}
 		return nil
 	}
 }
 
-func escapeAndCombineArgs(args []string) string {
+// WithAllCurrentCapabilities propagates the effective capabilities of the caller process to the container process.
+// The capability set may differ from [WithAllKnownCapabilities] when running in a container.
+func WithAllCurrentCapabilities(ctx context.Context, client Client, c *containers.Container, s *Spec) error {
+	caps, err := cap.Current()
+	if err != nil {
+		return err
+	}
+	return WithCapabilities(caps)(ctx, client, c, s)
+}
+
+// WithAllKnownCapabilities sets all the known linux capabilities for the container process.
+func WithAllKnownCapabilities(ctx context.Context, client Client, c *containers.Container, s *Spec) error {
+	return WithCapabilities(cap.Known())(ctx, client, c, s)
+}
+
+func escapeAndCombineArgs([]string) string {
 	panic("not supported")
 }
